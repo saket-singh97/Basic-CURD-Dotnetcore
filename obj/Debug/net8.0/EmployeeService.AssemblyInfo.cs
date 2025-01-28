02859d0484a23429bc13c415e70095f685993909
Saket <saket1448@gmail.com>
2025-01-28 11:57:23 +0530

    Done

--- obj/Debug/net8.0/EmployeeService.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b8b3955e5d2ebed38799a49dfde34c678df5fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648d6c6a3b6a15c6d8cd7603a8138641e348f96d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
